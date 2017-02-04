d1f229187953d0dfbf6d46dc32a77618b83d683a
GHackAnonymous <ederaraia@gmail.com>
2017-02-04 12:08:36 +0100

    Programa suma pares
    Programa que recorre del 1 al 10 todos los numeros y va sumando solo los que son pares.

---
+++ SumaPares10/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim resultado As Integer = 0
+        For i = 0 To 10
+            If (i Mod 2) = 0 Then
+                REM System.Console.WriteLine(i)
+                resultado = resultado + i
+            End If
+        Next
+        System.Console.WriteLine("El resultado de la suma de los")
+        System.Console.WriteLine("numeros pares del 1 al 10 es: " & resultado)
+        System.Console.ReadLine()
+    End Sub
+End Module
