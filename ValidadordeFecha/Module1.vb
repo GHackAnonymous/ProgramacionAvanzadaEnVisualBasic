fce5a8c309b95c545516882b4eb1fafce97d58df
GHackAnonymous <ederaraia@gmail.com>
2017-02-05 16:00:05 +0100

    Validador de fecha
    Programa que introduciendo una fecha la valida y la muesta si es valida si no da un mensage de error.

---
+++ ValidadordeFecha/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim fecha As Date
+
+        System.Console.Write("Dame una fecha (Ejem: 02/03/2014): ")
+        Try
+            fecha = (System.Console.ReadLine())
+            System.Console.WriteLine("La fecha introducida es: " & fecha)
+        Catch e As InvalidCastException
+            System.Console.WriteLine("Error: fecha no valida")
+        End Try
+
+        System.Console.ReadLine()
+
+    End Sub
+
+End Module
